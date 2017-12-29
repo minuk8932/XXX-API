8689639f85ba3d2697a05463e7d28124b709222e
minuk8932 <minuk8932@naver.com>
2017-12-29 18:41:58 +0900

    first settings

--- src/main/java/com/xxx/api/XXXApplication.java
+++ src/main/java/com/xxx/api/XXXApplication.java
@@ -1,4 +1,4 @@
-package com.datasaver.api;
+package com.xxx.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties
-public class DataSaverApplication {
+public class XXXApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DataSaverApplication.class, args);
+		SpringApplication.run(XXXApplication.class, args);
 	}
 }
