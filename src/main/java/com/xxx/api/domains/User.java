a14638f7d273c728d5175eec06a7c01d42cd9a4c
minuk8932 <minuk8932@naver.com>
2018-03-02 14:44:24 +0900

    add User table

--- src/main/java/com/xxx/api/domains/User.java
+++ src/main/java/com/xxx/api/domains/User.java
@@ -1,5 +1,7 @@
 package com.xxx.api.domains;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,4 +14,88 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "idx")
 	private long idx;
+
+	@Column(name = "id")
+	private String id;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "phoneNumber", unique = true)
+	private String phoneNumber;
+	
+	@Column(name = "ts", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private Timestamp ts;
+	
+	public User(long idx, String id, String password, String address, String email, String phoneNumber, Timestamp ts) {
+		this.idx = idx;
+		this.id = id;
+		this.password = password;
+		this.address = address;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.ts = ts;
+	}
+
+	public long getIdx() {
+		return idx;
+	}
+
+	public void setIdx(long idx) {
+		this.idx = idx;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Timestamp getTs() {
+		return ts;
+	}
+
+	public void setTs(Timestamp ts) {
+		this.ts = ts;
+	}
 }
