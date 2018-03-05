60c728e0d9cbab999edcd7c122b446a9f1379875
minuk8932 <minuk8932@naver.com>
2018-03-05 10:42:47 +0900

    add Notice table

---
+++ src/main/java/com/xxx/api/domains/Notice.java
@@ -0,0 +1,68 @@
+package com.xxx.api.domains;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Notice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idx")
+	private long idx;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "contents", columnDefinition = "TEXT")
+	private String contents;
+
+	@Column(name = "ts", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private Timestamp ts;
+
+	public Notice() {
+	}
+
+	public Notice(long idx, String title, String contents, Timestamp ts) {
+		this.idx = idx;
+		this.title = title;
+		this.contents = contents;
+		this.ts = ts;
+	}
+
+	public long getIdx() {
+		return idx;
+	}
+
+	public void setIdx(long idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public Timestamp getTs() {
+		return ts;
+	}
+
+	public void setTs(Timestamp ts) {
+		this.ts = ts;
+	}
+}
