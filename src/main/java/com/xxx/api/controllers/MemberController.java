25c08ab64119e9656297487e50eb24306d091bb3
minuk8932 <minuk8932@naver.com>
2018-04-08 15:15:14 +0900

    add MemberController

--- src/main/java/com/xxx/api/controllers/MemberController.java
+++ src/main/java/com/xxx/api/controllers/MemberController.java
@@ -1,5 +1,18 @@
 package com.xxx.api.controllers;
 
-public class MemberController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.xxx.api.services.MemberService;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("member")
+@Api
+public class MemberController {
+	@Autowired
+	private MemberService ms;
+	
 }
